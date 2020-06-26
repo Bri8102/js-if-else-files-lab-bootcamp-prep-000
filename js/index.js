5d8bd8a630ca0e5f0ad6cf77d33f5b5cb90e72e9
Briana Oniru <damioniru16@gmail.com>
2020-06-26 20:20:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan") {
+  console.log('the namme is correct')
+} else {
+  console.log('Expected the name to be susan')
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
